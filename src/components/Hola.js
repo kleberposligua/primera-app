ab695ba0581d5d184b63199504222c68e7d4a968
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2022-02-07 12:39:30 -0500

    Ejemplo deestructuración

--- src/components/Hola.js
+++ src/components/Hola.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Hola(props) {
+function Hola({mensaje, pie}) {
     return (
         <div>
             <h1>PUCESE</h1>
-            <h2>{props.mensaje}</h2>
-            <h3>{props.pie}</h3>
+            <h2>{mensaje}</h2>
+            <h3>{pie}</h3>
         </div>
     );
 }
